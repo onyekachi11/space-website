e84e32a714e3958b3a1d2f3d85703aa8399c1e30
onyekachi <emegwaonyeka@gmail.com>
2023-05-21 20:00:30 +0100

    new errors fixed

--- src/main.jsx
+++ src/main.jsx
@@ -8,9 +8,9 @@ import { BrowserRouter} from "react-router-dom";
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
       <BrowserRouter>
-      <Context>
+      {/* <Context> */}
         <App />
-      </Context>
+      {/* </Context> */}
       </BrowserRouter>
     </React.StrictMode>
 )
