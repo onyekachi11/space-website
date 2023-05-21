aae98de7114fd05f03babf47941b7f09a2dfb435
onyekachi <emegwaonyeka@gmail.com>
2023-05-21 19:56:37 +0100

    fixed context error

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React, {useContext} from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
-import { Context } from './component/context'
+// import { Context } from './component/context'
 import { BrowserRouter} from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
